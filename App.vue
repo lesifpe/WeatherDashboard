fb89181c7a2072aeb8e7b1947d4f63ce0a713b5d
Victor Soares <victor.user.scds@gmail.com>
2026-08-16 11:45:37 -0300

    feat(view): criação de uma pagina home, criação de um arquivo css global e atualização dos componentes vue

--- App.vue
+++ App.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="layoutPrincipal">
-    <header>
-      <h1>Data Insights</h1>
-    </header>
+  <div class="layout-principal">
+    <Navbar />
     
     <main>
-      <!-- Onde as páginas da pasta view serão exibidas -->
       <HomeView />
     </main>
+    
+    <Footer />
   </div>
 </template>
 
 <script setup>
+import Navbar from './components/ui/Navbar.vue'
+import Footer from './components/ui/Footer.vue'
 import HomeView from './view/HomeView.vue'
 </script>
-
-<style scoped>
-.layout-principal {
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 20px;
-}
-</style>
