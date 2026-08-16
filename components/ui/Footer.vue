ceab33a7bcf273375dd4207ae3204452b4758baa
Victor Soares <victor.user.scds@gmail.com>
2026-08-16 11:44:12 -0300

    feat(components): criação do footer e do navbar

---
+++ components/ui/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="footer">
+    <p>&copy; 2026 Data Insights — LES IFPE Recife.</p>
+  </footer>
+</template>
---
+++ components/ui/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar">
+    <div class="nav-logo">
+      <span class="text-gradient">📊</span>
+      <span class="text-gradient">Data Insights</span>
+    </div>
+    
+    <ul class="nav-links">
+      <li><a href="#" class="active">Início</a></li>
+    </ul>
+    <button class="theme-toggle" aria-label="Alternar Tema">
+      🌙
+    </button>
+  </nav>
+</template>
