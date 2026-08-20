cdc5becd586606104f00aa8266865844726ede81
Márcio Luan <marcioluan18@hotmail.com>
2026-08-20 12:31:07 -0300

    feat: criação de tela contato e adição de formulário

---
+++ view/Contato.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class = "contato-page">
+        <header class = "contato-header">
+            <h1 class = "texto-gradiente">Entre em contato</h1>
+            <p>Quer adicionar seu projeto ao Data Insights? Preencha o formulário abaixo</p>
+        </header>  
+
+        <div class = "formulario">
+            <Formulario />
+
+
+        </div>
+        
+        
+
+        
+    </section>
+
+
+</template>
+
+<script setup>
+    import Formulario from '../components/ui/Formulario.vue';
+</script>
