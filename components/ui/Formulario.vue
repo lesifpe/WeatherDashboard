ec1ea6ac5f1d7a3f1e2da07dc14cef2e81c8ab65
Márcio Luan <marcioluan18@hotmail.com>
2026-09-03 10:49:06 -0300

    feat: Estilização da página de contato

--- components/ui/Formulario.vue
+++ components/ui/Formulario.vue
@@ -3,19 +3,19 @@
         <h3>Solicitar adição de projeto</h3>
 
         <form class = "formulario" @submit.prevent="enviarFormulario">
-            <label for ="nome">Nome completo</label><br>
+            <label for ="nome"><img src="../../assets/user_black.png" alt="Icone usuário" class="form-img">   Nome completo</label><br>
             <input type = "text" placeholder = "Insira seu nome" id = "nome" v-model="nome"><br>
             
-            <label for = "email">Email</label><br>
+            <label for = "email"><img src="../../assets/mail_black.png" alt="Icone email" class="form-img">   Email</label><br>
             <input type = "email" placeholder = "seu@email.com" id = "email" v-model="email"><br>
             
-            <label for="nomeProjeto">Nome do projeto</label><br>
+            <label for="nomeProjeto"><img src="../../assets/file-text_black.png" alt="Icone projeto" class="form-img">   Nome do projeto</label><br>
             <input type = "text" placeholder = "Nome do seu projeto" id = "nomeProjeto" v-model="nomeProjeto"><br>
             
             <label for="descricao">Descrição do projeto</label><br>
             <textarea rows="8" cols = "50" placeholder="Descreva seu projeto, os dados que você gostaria de visualizar e qualquer informação relevante..." id = "descricao" v-model="descricao"></textarea><br>
 
-            <button type = "submit" class="bto-primario">Enviar solicitação</button>
+            <button type = "submit" class="bto-primario"><img src="../../assets/send_white.png" alt="Icone enviar" class="form-img">   Enviar solicitação</button>
         </form>
     </div>
 </template>
--- view/Contato.vue
+++ view/Contato.vue
@@ -1,6 +1,7 @@
 <template>
     <section class = "contato-page">
         <header class = "contato-header">
+            <img src="../assets/mail_blue.png" alt = "Icone mensagem" class="contato-img">
             <h1 class = "texto-gradiente">Entre em contato</h1>
             <p id="subtitulo">Quer adicionar seu projeto ao Data Insights? Preencha o formulário abaixo</p>
         </header>  
@@ -12,21 +13,21 @@
             <div class = "cards-direita">
                 <article class = "card funcionamento">
                     <h3>Como funciona?</h3>
-                    <h3>Envie sua solicitação</h3>
+                    <h3><span class="numero">1</span>   Envie sua solicitação</h3>
                     <p>Preencha o formulário com informações sobre seu projeto e os dados que deseja visualizar.</p>
                     
-                    <h3>Análise da Equipe LES</h3>
+                    <h3><span class="numero">2</span>   Análise da Equipe LES</h3>
                     <p>Nossa equipe avaliará a viabilidade técnica e o alinhamento com os objetivos da plataforma.</p>
-                    <h3>Implementação</h3>
+                    <h3><span class="numero">3</span>   Implementação</h3>
                     <p>Após aprovação, trabalharemos na integração dos dados e criação do dashboard personalizado.</p>
                 </article>
 
                 <article class="card requisitos">
                     <h3>Requisitos</h3>
-                    <p>Dados estruturados e organizados</p>
-                    <p>Relevância acadêmica ou institucional</p>
-                    <p>Permissão para uso e visualização dos dados</p>
-                    <p>Disponibilidade para colaboração técnica</p>
+                    <p><img src="../assets/check_green.png" alt ="Icone check" class = "check-img"> Dados estruturados e organizados</p>
+                    <p><img src="../assets/check_green.png" alt ="Icone check" class = "check-img">Relevância acadêmica ou institucional</p>
+                    <p><img src="../assets/check_green.png" alt ="Icone check" class = "check-img">Permissão para uso e visualização dos dados</p>
+                    <p><img src="../assets/check_green.png" alt ="Icone check" class = "check-img">Disponibilidade para colaboração técnica</p>
                 </article>
 
                 <article class = "card nota">
